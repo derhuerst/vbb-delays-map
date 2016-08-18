3b63a8a9256ad94847df6f5ddca4bc862f9929c3
Jannis R <mail@jannisr.de>
2016-08-18 02:33:06 +0200

    map :round_pushpin:

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 'use strict'
 
-// todo
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ3JlZndkYSIsImEiOiJjaXBxeDhxYm8wMDc0aTZucG94d29zdnRyIn0.oKynfvvLSuyxT3PglBMF4w'
+const map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/dark-v9',
+	zoom: 12,
+	hash: true,
+	center: [13.4, 51.4]
+})
+map.addControl(new mapboxgl.Navigation())
+
+
+
+const el = document.getElementById('map')
+
+const resize = () => {
+	const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+	const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+	el.style.width = w + 'px'
+	el.style.height = h + 'px'
+	map.resize()
+}
+resize()
+window.addEventListener('resize', resize)
+
+
+
+map.addSource('some id', {data: '//stations.geojson'})
